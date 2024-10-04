1dd1ffe9ff6655d349d9e38b1000cdbad038c611
jomm <joemired@gmail.com>
2024-10-04 17:49:06 +0800

    sdfsd

--- src/main/java/com/example/hql/HelloApplication.java
+++ src/main/java/com/example/hql/HelloApplication.java
@@ -4,14 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
+
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("/fonts/VampireWars.ttf"), 55);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
